7a7f5fa571a99c546746dbd6edc736aca99fba31
sharayu-savale <sharayusavale34@gmail.com>
2026-08-04 23:34:36 +0530

    Update linear search.c

--- linear search.c
+++ linear search.c
@@ -11,20 +11,3 @@ scanf("%d",&arr[i]);
 printf("enter element to search:");
 scanf("%d",&key);
 p=arr;
-for(i=0;i<n;i++)
-{
- if(*(p+i)==key)
- {
-  printf("element found at position=%d",i+1);
-  return 0;
- }
-}
-printf("element not found");
-
-  
-
-
-
-
-return 0;
-}
