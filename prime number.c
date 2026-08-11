ccc6e5775eeb6ce8aca11b6ba8dbccc7df83b8d3
sharayu-savale <sharayusavale34@gmail.com>
2026-08-11 22:11:59 +0530

    Add prime number program

---
+++ prime number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("It is not a prime number\n");
+    }
+    else {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("It is a prime number\n");
+        else
+            printf("It is not a prime number\n");
+    }
+
+    return 0;
+}
